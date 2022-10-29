c556f6ef1c5d68b92e0a88b415918f4ff9a58ad5
bafreer <116974831+bafreer@users.noreply.github.com>
2022-10-29 14:48:55 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+for (let index = 0; index < 4; index++) {
+    music.setVolume(53)
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.setVolume(113)
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.setVolume(212)
+    music.playTone(494, music.beat(BeatFraction.Whole))
+}
+basic.forever(function () {
+	
+})
