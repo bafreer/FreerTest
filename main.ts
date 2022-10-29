cd3a2f40cb717ebc0326806cfd8dfd5ee2d3686d
bafreer <116974831+bafreer@users.noreply.github.com>
2022-10-29 14:52:39 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ for (let index = 0; index < 4; index++) {
     music.setVolume(53)
     music.playTone(494, music.beat(BeatFraction.Whole))
     music.setVolume(113)
-    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
     music.setVolume(212)
-    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.playTone(988, music.beat(BeatFraction.Whole))
 }
 basic.forever(function () {
 	
